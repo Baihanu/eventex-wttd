3a024bc68d9138f171a08edc4ae24b5e1234e878
Lucas <lucasabreusilva.98@gmail.com>
2021-03-01 15:54:20 -0300

    Configurado allowed_hosts

--- eventexx/urls.py
+++ eventexx/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import eventexx.core.views
+from eventexx.core.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', eventexx.core.views.home),
+    path('', home),
 ]
